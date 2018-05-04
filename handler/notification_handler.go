434a09ec592de994b2481cc7bd67606802beba7b
Fréjus Gbaguidi <fh.gbaguidi@gmail.com>
2018-05-04 03:09:36 +0200

    Update notification_handler.go

--- handler/notification_handler.go
+++ handler/notification_handler.go
@@ -7,10 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetAllNotificationsHandler handle requests to show all notifications
 func GetAllNotificationsHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, model.Notifications)
 }
 
+// GetNotificationHandler handle requests to show a notification using its id
 func GetNotificationHandler(c *gin.Context) {
 	id := c.Param("id")
 	for _, item := range model.Notifications {
